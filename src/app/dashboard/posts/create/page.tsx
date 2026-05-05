15ecee08ff4acfaaa66233863b4630893d48efc7
mizuki-iijima <zima.web05@gmail.com>
2026-05-05 23:14:34 +0900

    PostFormコンポーネントを使うように修正

--- src/app/dashboard/posts/create/page.tsx
+++ src/app/dashboard/posts/create/page.tsx
@@ -1,48 +1,20 @@
-'use client';
-
-import {
-  createPostAction,
-  type CreatePostState,
-} from "@/src/app/dashboard/posts/actions";
-import MarkdownEditor from "@/src/components/MarkdownEditor";
-import { useActionState, useState } from "react";
-
+import PostForm from "@/src/components/PostForm";
 
 export default function CreatePost() {
-  const [content, setContent] = useState('');
-  const initialState: CreatePostState = {
-    success: false,
-    message: "",
-    errors: {},
-  };
-  const [state, postAction] = useActionState(
-    createPostAction, 
-    initialState
-  );
-
   const panelClassName = "rounded-lg border border-gray-300 bg-white px-4 py-6";
 
   return (
     <>
       <div className="p-6 grow">
-        {state.message && <p className="absolute left-1/2 w-100 -translate-x-1/2 rounded-[10px] border-2 border-[#afefaf] p-1.25 text-center">{state.message}</p>}
         <div className="mt-6">管理画面 › 記事作成</div>
         <h1 className="mt-2 font-bold text-3xl">記事作成</h1>
         <div className="flex justify-between gap-10 items-start">
-          <form action={postAction} className="mt-6 space-y-5 max-w-5xl grow">
-            <div className="flex flex-col gap-2">
-              <label htmlFor="title">タイトル</label>
-              <input type="text" name="title" id="title" className="rounded-md border border-slate-300 bg-white px-3 py-2 text-sm" required />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="content">本文</label>
-              <input type="hidden" name="content" value={content} />
-              <div className="rounded-lg border border-gray-300 bg-white">
-                <MarkdownEditor value={content} onChange={setContent}/>
-              </div>
-            </div>
-            <button>作成</button>
-          </form>
+          
+          <PostForm 
+            mode="create"
+            postTitle=""
+            postContent=""
+          />
 
           <div className="grow">
             <div className={`${panelClassName} space-y-4`}>
