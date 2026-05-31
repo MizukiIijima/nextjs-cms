cb1b94fd216afdbb580c4cac76cb2a14534db178
mizuki-iijima <zima.web05@gmail.com>
2026-05-31 17:28:17 +0900

    ボタンコンポーネントのプロパティに「outline」バリアントを追加

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,17 @@
 type ButtonProps = {
   children: React.ReactNode;
-  variant: "primary" | "danger" | "default";
+  variant: "primary" | "danger" | "default" | "outline";
   type?: "button" | "submit";
   className?: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
-}
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
 
 const buttonStyle = {
   primary: "text-white bg-primary hover:bg-primary-dark",
   danger: "text-white bg-danger hover:bg-danger-dark",
   default: "bg-default hover:bg-default-dark",
-}
+  outline: "border border-slate-200 bg-white text-slate-700 hover:bg-slate-50",
+};
 
 export default function Button({ children, variant, type = "button", className, onClick }: ButtonProps) {
   return (
