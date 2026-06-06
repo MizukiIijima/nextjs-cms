7803b3192a7110cdb4f79c2067a750f3156a528a
mizuki-iijima <zima.web05@gmail.com>
2026-06-06 20:35:55 +0900

    投稿編集ページのコードを整理

--- src/app/(admin)/dashboard/posts/[id]/page.tsx
+++ src/app/(admin)/dashboard/posts/[id]/page.tsx
@@ -31,33 +31,6 @@ export default async function PostEditPage({ params }: { params: Promise<{ id: n
           allTags={allTags}
         />
 
-        <div className="grow">
-          <div className={`${panelClassName} space-y-4`}>
-            <h2 className="font-bold text-xl">公開設定</h2>
-            <div className="flex gap-3">
-              <p>状態</p>
-              <p>valuevalue</p>
-            </div>
-            <div className="flex gap-3">
-              <p>公開日</p>
-              <p>valuevalue</p>
-            </div>
-            <div className="flex gap-3">
-              <p>更新日</p>
-              <p>valuevalue</p>
-            </div>
-          </div>
-
-          <div className={`${panelClassName} mt-5 space-x-3`}>
-            <h2 className="font-bold text-xl">カテゴリ</h2>
-            
-          </div>
-
-          <div className={`${panelClassName} mt-5 space-x-3`}>
-            <h2 className="font-bold text-xl">アイキャッチ画像</h2>
-            
-          </div>
-        </div>
       </div>
     </div>
   )
